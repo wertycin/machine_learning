a16bdf78eda46c534dd404ead31ff54ed28dad9e
Иван Руднев <wertycin@gmail.com>
2023-09-01 19:09:50 +0300

    add colors

--- RFC.py
+++ RFC.py
@@ -1,10 +1,11 @@
 # Иллюстрация работы Random Forest Classifier
 from sklearn.ensemble import RandomForestClassifier
+from module import *
 clf = RandomForestClassifier(random_state=0)
-N = 10 # Размеры прямоугольного поля
-M = 20
-X = [[0,  0], [9, 9], [0, 9]]
-y = [0, 1, 2]
+N = 40 # Размеры прямоугольного поля
+M = 80
+X = [[0,  0], [38, 25], [0, 9], [10, 18], [20, 20], [38, 10]]
+y = [0, 1, 2, 3, 4, 5]
 clf.fit(X, y)
 table = [[0 for i in range(M)] for i in range(N)]
 values = [[0, 0] for i in range(N * M)]
@@ -19,17 +20,19 @@ for i in range(N):
     for j in range(M):
         table[i][j] = predict[k]
         k += 1
+
+
 for i in range(M + 2):
     print('.', end='')
 print()
 for i in range(N):
     print('.', end='')
     for j in range(M):
-        flag = 0
+        flag = -1
         for k in range(len(X)):
-            if X[k] == [i, j]: flag = 1
-        if flag: print('*', end = "")
-        else: print(table[i][j], end ="")
+            if X[k] == [i, j]: flag = k
+        if flag != -1: print(colours[table[i][j]].format(flag), end = "")
+        else: print(colours[table[i][j]].format(" "), end = "")
     print('.')
 
 for i in range(M + 2):
